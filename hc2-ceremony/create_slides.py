37a11a2cb7740311396bc2befd863bbe3a7fa3dc
Titus Cieslewski <dev@titus-c.ch>
2015-03-21 16:58:48 +0100

    Updates to create_slides.py Now adds a photo to each slide Adds "*" to each "justforfun" team name

--- hc2-ceremony/create_slides.py
+++ hc2-ceremony/create_slides.py
@@ -2,11 +2,15 @@
 import csv, re, os, sys
 
 def is_number(s):
-    try:
-        float(s)
-        return True
-    except ValueError:
-        return False
+  try:
+    float(s)
+    return True
+  except ValueError:
+    return False
+    
+#DSC_0356.JPG
+#file://HC2PHOTOLIBRARY
+
 
 # ranks format: | team id | team name | rank | score | penalties | timepenalty | photo | justforfun? | affiliation | names, separated by comma
 with open('ranks.csv', 'rb') as csvfile:
@@ -18,11 +22,12 @@ with open('ranks.csv', 'rb') as csvfile:
     row[3], # score
     row[4], # penalties
     row[5], # time
+    row[6], # photo
     row[7], # category
     [name.strip() for name in row[9].split(",")] # team members
     ) for row in rankreader]
-
-student_scores = sorted([score for score in scores if score[-2] == "0"],key=lambda s:s[2])
+  
+student_scores = sorted([score for score in scores],key=lambda s:s[2])
 
 def clean(string):
   return re.sub(r"[^a-zA-Z0-9.]", "_", string)
@@ -30,7 +35,7 @@ def clean(string):
 def create_slides(scores, max_rank):
   pdfnames = []
   for score in scores:
-    teamid,team,rank,score,penalties,time,cat,players = score
+    teamid,team,rank,score,penalties,time,photo,cat,players = score
     if rank > max_rank:
       break
     # fill in template
@@ -38,11 +43,13 @@ def create_slides(scores, max_rank):
     print filename
     with open('out/'+filename+'.svg','w') as new_file, open('template-2015.svg') as old_file:
       for line in old_file:
-        new_file.write(line.replace('NAMESNAMES', "<![CDATA["+'\n'.join(players)+"]]>").replace('RANKRANK', ("And finally, on" if (rank==1) else "On")+" rank "+str(rank)).replace('TEAMTEAM', "<![CDATA["+team+"]]>").replace('POINTSPOINTS', "With "+str(score)+" points, a total time of "+str(time)+" and a penalty of "+penalties))
+        new_file.write(line.replace('NAMESNAME1', "<![CDATA["+players[0]+"]]>").replace('NAMESNAME2', "<![CDATA["+(players[1] if len(players) >= 2 else "")+"]]>").replace('NAMESNAME3', "<![CDATA["+(players[2] if len(players) >= 3 else "")+"]]>").replace('RANKRANK', ("And finally, on" if (rank==1) else "On")+" rank "+str(rank)).replace('TEAMTEAM', "<![CDATA["+(team+'*' if (cat == '1') else team)+"]]>").replace('POINTSPOINTS', "With "+str(score)+" "+("point" if (score=='1') else "points")+", a total time of "+str(time)+" and a penalty of "+penalties).replace('HC2PHOTOLIBRARY', os.path.join(os.path.abspath('.'),'photos-2015','DSC_0'+photo+'.JPG')))
     
     # Convert svg to pdf
-    print 'rsvg-convert -f pdf -o out/'+filename+'.pdf out/'+filename+'.svg'
-    os.system('rsvg-convert -f pdf -o "out/'+filename+'.pdf" "out/'+filename+'.svg"')
+    #print 'rsvg-convert -f pdf -o out/'+filename+'.pdf out/'+filename+'.svg'
+    #os.system('rsvg-convert -f pdf -o "out/'+filename+'.pdf" "out/'+filename+'.svg"')
+    print 'inkscape out/'+filename+'.svg --export-pdf=out/'+filename+'.pdf'
+    os.system('inkscape out/'+filename+'.svg --export-pdf=out/'+filename+'.pdf')
     
     pdfnames.append('out/'+filename+'.pdf')
   
@@ -55,5 +62,5 @@ def create_slides(scores, max_rank):
   os.system(catcommand)
 
 os.system('mkdir -p out')
-create_slides(student_scores, 6)
+create_slides(student_scores, 30)
 
